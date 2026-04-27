18d7ddd7e38221484671d25d6e7a07f138f5046b
Sneh <snehsohagiya117@gmail.com>
2026-04-27 11:08:31 +0530

    ch-1

---
+++ Ch-1 Introduction To Cpp/first.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+   
+     cout << "Name    :sneh" << endl;
+     cout << "Age     :22" << endl;
+     cout << "college :BMU" << endl;
+
+   
+     return 0;
+
+
+}
