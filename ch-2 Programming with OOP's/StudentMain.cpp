08f2a3a69ff97ecaa042fe175befcd2ed1febdbe
Sneh <snehsohagiya117@gmail.com>
2026-04-30 11:00:57 +0530

    ch-1'

---
+++ ch-2 Programming with OOP's/StudentMain.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "Student.cpp"
+
+int main ()
+{
+
+    Student s1, s2;
+
+    s1.setStudentDetail();
+    s2.setStudentDetail();
+
+
+    s1.getStudentDetail();
+    s2.getStudentDetail();
+
+    return 0;
+}
