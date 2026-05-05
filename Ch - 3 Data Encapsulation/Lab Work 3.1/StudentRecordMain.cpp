49d011d742b86fcc34de5bf4d5212c55db990aac
Sneh Sohagiya <snehsohagiya117@gmail.com>
2026-05-05 11:07:01 +0530

    Add files via upload

---
+++ Ch - 3 Data Encapsulation/Lab Work 3.1/StudentRecordMain.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "Q1-Student_Record.cpp"
+
+int main ()
+{
+
+    StudentRecord s1, s2, s3, s4, s5;
+
+    s1.setStudentRecord();
+    s2.setStudentRecord();
+    s3.setStudentRecord();
+    s4.setStudentRecord();
+    s5.setStudentRecord();
+
+    return 0;
+}
